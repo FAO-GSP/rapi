86d191054ce343a236084ca3fb4c7513173533c0
midraed <guillermo.olmedo@gmail.com>
2019-01-04 19:15:52 -0300

    test xyplot()

--- api.R
+++ api.R
@@ -48,16 +48,17 @@ function(req){
 function(req){
   spc <- jsonToAqp(req$postBody)
   agg <- aqp::slab(spc, fm= ~ organic_carbon + ph_h2o_1 + ecec + clay +silt + sand)
-  xyplot(top ~ p.q50 | variable, data=agg, ylab='Depth',
-         xlab='median bounded by 25th and 75th percentiles',
-         lower=agg$p.q25, upper=agg$p.q75, ylim=c(150,-2),
-         panel=panel.depth_function,
-         alpha=0.25, sync.colors=TRUE,
-         par.settings=list(superpose.line=list(col='RoyalBlue', lwd=2)),
-         prepanel=prepanel.depth_function,
-         cf=agg$contributing_fraction, cf.col='black', cf.interval=5,
-         layout=c(6,1), strip=strip.custom(bg=grey(0.8)),
-         scales=list(x=list(tick.number=4, alternating=3, relation='free')))
+  # xyplot(top ~ p.q50 | variable, data=agg, ylab='Depth',
+  #        xlab='median bounded by 25th and 75th percentiles',
+  #        lower=agg$p.q25, upper=agg$p.q75, ylim=c(150,-2),
+  #        panel=panel.depth_function,
+  #        alpha=0.25, sync.colors=TRUE,
+  #        par.settings=list(superpose.line=list(col='RoyalBlue', lwd=2)),
+  #        prepanel=prepanel.depth_function,
+  #        cf=agg$contributing_fraction, cf.col='black', cf.interval=5,
+  #        layout=c(6,1), strip=strip.custom(bg=grey(0.8)),
+  #        scales=list(x=list(tick.number=4, alternating=3, relation='free')))
+  plot(agg)
 }
 
 #* Pair-Wise Dissimilarity
