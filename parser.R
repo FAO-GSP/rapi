63d42ea2324326f816633ef87add49544813b848
midraed <guillermo.olmedo@gmail.com>
2019-01-04 17:52:26 -0300

    error handling for empty profiles

--- parser.R
+++ parser.R
@@ -9,13 +9,16 @@ jsonToAqp <- function(json){
   names(sites) <- c("profile_id", "identifier", "X", "Y", "country_code")
   # bind 1 data.frame per profile into a big data.frame
   spc <- do.call(rbind, json$properties.layers)
-  # fix numeric variables loaded as strings
-  spc[5:18] <- lapply(spc[5:18], as.numeric)
-  # convert layers data.frame to spc
-  depths(spc) <- profile_id ~ top + bottom
-  # add site properties to spc
-  site(spc) <- sites
-  # upgrade XY to spatial coordinates
-  coordinates(spc) <- ~ X + Y
-  return(spc)
+  if(is.data.frame(spc)){
+    # fix numeric variables loaded as strings
+    spc[5:18] <- lapply(spc[5:18], as.numeric)
+    # convert layers data.frame to spc
+    depths(spc) <- profile_id ~ top + bottom
+    # add site properties to spc
+    site(spc) <- sites
+    # upgrade XY to spatial coordinates
+    coordinates(spc) <- ~ X + Y
+    return(spc)} else {
+      stop("Soil profiles are empty!")
+    }
 }
