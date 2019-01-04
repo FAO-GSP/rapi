3fec985b8f2de17d468430bb928bc2446aeeb5c7
midraed <guillermo.olmedo@gmail.com>
2019-01-03 22:11:15 -0300

    fix layers properties from strings to numbers

--- parser.R
+++ parser.R
@@ -5,6 +5,7 @@ jsonToAqp <- function(json){
   sites <- as.data.frame(sites)
   names(sites) <- c("profile_id", "identifier", "X", "Y", "country_code")
   spc <- do.call(rbind, json$properties.layers)
+  spc[5:18] <- lapply(spc[5:18], as.numeric)
   depths(spc) <- profile_id ~ top + bottom
   site(spc) <- sites
   coordinates(spc) <- ~ X + Y
